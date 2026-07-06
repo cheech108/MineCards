f4bdb39fbd499d21405a266068547a134cdfa18e
frank <fadamato20@gmail.com>
2026-07-06 14:02:06 -0400

    added givepack command

--- src/main/java/dev/cheech/MineCards.java
+++ src/main/java/dev/cheech/MineCards.java
@@ -5,11 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MineCards extends JavaPlugin {
 
+    static JavaPlugin instance;
     @Override
     public void onEnable() {
         // Plugin startup login
         new PackManager(getDataFolder());
         getServer().getPluginManager().registerEvents(new PackOpen(), this);
         getCommand("givepack").setExecutor(new GivePack());
+        instance = this;
+    }
+
+    public static JavaPlugin getPluginObj(){
+        return instance;
     }
 }
--- src/main/java/dev/cheech/commands/GivePack.java
+++ src/main/java/dev/cheech/commands/GivePack.java
@@ -1,11 +1,17 @@
 package dev.cheech.commands;
 
+import dev.cheech.MineCards;
 import dev.cheech.PackManager;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -16,7 +22,18 @@ import java.util.List;
 public class GivePack implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        return false;
+        ItemStack itemStack = new ItemStack(Material.PAPER);
+        // 1. Get Meta and Define Key
+        ItemMeta meta = itemStack.getItemMeta();
+        NamespacedKey key = new NamespacedKey(MineCards.getPluginObj(), "pack_name");
+
+        // 2. Set Data (e.g., String, Integer, Byte)
+        meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, args[1]);
+
+        // 3. Apply and Give
+        itemStack.setItemMeta(meta);
+        Bukkit.getPlayer(args[0]).getInventory().addItem(itemStack);
+        return true;
     }
 
     @Override
